54385efb24dc48749966aca0781328953b9367e0
Cade <46201042+KinCadeFoster@users.noreply.github.com>
2024-06-04 00:39:32 +0400

    Stage_2 - Инициализирован массив dictionary - На каждой итерации прохода по строкам файла создаём объект типа Word и добавляем его в dictionary - Добавил информацию о количестве правильных ответов в файл words.txt и в сущность Word - Распечатал содержимое dictionary в консоль - Добавил обработку ошибок прасинга строк, что бы мы могли обработать валидные строки при возникновении ошибок

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,24 @@
 import java.io.File
 
+data class Word(
+    val enWord: String,
+    val ruWord: String,
+    val correctCount: Int,
+)
+
+
 fun main() {
+    val dictionary: MutableList<Word> = mutableListOf()
     val wordsFile = File("words.txt")
 
-    for (string in wordsFile.readLines())
-    println(string)
+    for (string in wordsFile.readLines()) {
+        try {
+            val lineSplit = string.split(" | ")
+            dictionary.add(Word(enWord = lineSplit[0], ruWord = lineSplit[1], correctCount = lineSplit[2].toInt()))
+        } catch (e: Exception) {
+            println("Ошибка чтения строки: \"$string\", строка будет пропущена")
+            println(e)
+        }
+    }
+    println(dictionary)
 }
