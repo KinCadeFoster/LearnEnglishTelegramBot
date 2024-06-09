a00865bc8bdad4bacfa868636e90fb306b2a193d
Cade <46201042+KinCadeFoster@users.noreply.github.com>
2024-06-09 05:14:23 +0400

    Stage_3 - Вынес логику в фунции getUserStatistics и getDictionary - Создал консольное меню будущего тренажера в цикле. Цикл обрабатывает ввод цифр 2 и 0 - При вводе чего угодно кроме 1, 2 или 0 – выводится сообщение "Неверный ввод. Введите доступные значения: 1, 2 или 0." - Реализовал пункт меню “Статистика”, с выводом в консоль

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,11 +6,20 @@ data class Word(
     val correctCount: Int,
 )
 
+fun getUserStatistics(dictionary: MutableList<Word>) {
+    val wordCount = dictionary.size
+    val learnedWords = dictionary.count { it.correctCount > 2 }
+    if (wordCount > 0) {
+        val percentageLearned = ((learnedWords.toDouble() / wordCount) * 100).toInt()
+        println("Выучено $learnedWords из $wordCount слов | $percentageLearned%")
+    } else {
+        println("Словарь пустой!")
+    }
+}
 
-fun main() {
+fun getDictionary(): MutableList<Word> {
     val dictionary: MutableList<Word> = mutableListOf()
     val wordsFile = File("words.txt")
-
     for (string in wordsFile.readLines()) {
         val lineSplit = string.split("|")
         if (lineSplit.size == 3)
@@ -18,9 +27,41 @@ fun main() {
                 Word(
                     enWord = lineSplit[0].trim(),
                     ruWord = lineSplit[1].trim(),
-                    correctCount = lineSplit[2].toIntOrNull() ?: 0
+                    correctCount = lineSplit[2].trim().toIntOrNull() ?: 0
                 )
             )
     }
-    println(dictionary)
+    return dictionary
+}
+
+
+fun main() {
+    val dictionary = getDictionary()
+
+    while (true) {
+        println("Выберите пункт меню:")
+        println("1. Учить слова")
+        println("2. Статистика")
+        println("0. Выход")
+
+        val userInput = readlnOrNull()
+
+        when (userInput) {
+            "1" -> {
+            }
+
+            "2" -> {
+                getUserStatistics(dictionary)
+            }
+
+            "0" -> {
+                println("Выход из программы...")
+                break
+            }
+
+            else -> {
+                println("Неверный ввод. Введите доступные значения: 1, 2 или 0.")
+            }
+        }
+    }
 }
