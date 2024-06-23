33a4e3f6bbc814d9a687183e8666c1857eaf9961
Cade <46201042+KinCadeFoster@users.noreply.github.com>
2024-06-24 00:16:30 +0400

    Stage_6 - рефакторинг всего кода

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,122 +1,6 @@
-import java.io.File
-
-const val WORD_LEARNED_LIMIT = 3
-const val TOTAL_ANSWER_CHOICES = 4
-const val NEEDED_WRONG_ANSWERS = TOTAL_ANSWER_CHOICES - 1
-
-data class Word(
-    val enWord: String,
-    val ruWord: String,
-    var correctCount: Int,
-)
-
-fun getUserStatistics(dictionary: List<Word>) {
-    val wordCount = dictionary.size
-    if (wordCount > 0) {
-        val learnedWords = dictionary.count { it.correctCount >= WORD_LEARNED_LIMIT }
-        val percentageLearned = ((learnedWords.toDouble() / wordCount) * 100).toInt()
-        println("Выучено $learnedWords из $wordCount слов | $percentageLearned%")
-    } else {
-        println("Словарь пустой!")
-    }
-}
-
-fun getDictionary(): List<Word> {
-    val dictionary: MutableList<Word> = mutableListOf()
-    val wordsFile = File("words.txt")
-    for (string in wordsFile.readLines()) {
-        val lineSplit = string.split("|")
-        if (lineSplit.size == 3)
-            dictionary.add(
-                Word(
-                    enWord = lineSplit[0].trim(),
-                    ruWord = lineSplit[1].trim(),
-                    correctCount = lineSplit[2].trim().toIntOrNull() ?: 0
-                )
-            )
-    }
-    return dictionary
-}
-
-fun updateWordsFile(dictionary: List<Word>) {
-    val file = File("words.txt")
-    file.printWriter().use { writer ->
-        dictionary.forEach { word ->
-            writer.println("${word.enWord}|${word.ruWord}|${word.correctCount}")
-        }
-    }
-}
-
-fun learnWords(dictionary: List<Word>) {
-    while (true) {
-        val unlearnedWords = getUnlearnedWords(dictionary)
-        if (unlearnedWords.isEmpty()) {
-            println("Вы выучили все слова!")
-            break
-        }
-        val learningWord = takeLearningWord(unlearnedWords)
-        println("Перевод слова <${learningWord.enWord.lowercase().replaceFirstChar { it.uppercase() }}>")
-
-        val answers = takeShuffleElements(dictionary, unlearnedWords, value = NEEDED_WRONG_ANSWERS, learningWord)
-        answers.forEach { it ->
-            println(
-                "${answers.indexOf(it) + 1}. ${it.ruWord.lowercase().replaceFirstChar { it.uppercase() }}"
-            )
-        }
-        println("0. Выход")
-
-        print("Введите ваш ответ: ")
-        val userAnswer = readlnOrNull()?.toInt() ?: 404
-        if (userAnswer == 0) break
-        if (userAnswer !in 1..TOTAL_ANSWER_CHOICES) {
-            println("Вы ввели неверное значение введите ответ от 1 до 4")
-        } else {
-            if (answers[userAnswer - 1].ruWord == learningWord.ruWord) {
-                learningWord.correctCount++
-                updateWordsFile(dictionary)
-            } else {
-                println(
-                    "Не верно, перевод слова <" +
-                            "${learningWord.enWord.lowercase().replaceFirstChar { it.uppercase() }}> " +
-                            "это <${learningWord.ruWord.lowercase().replaceFirstChar { it.uppercase() }}>"
-                )
-            }
-        }
-    }
-}
-
-
-
-fun takeLearningWord(unLearningWord: List<Word>): Word {
-    return unLearningWord.shuffled().take(1).first()
-}
-
-fun takeShuffleElements(
-    dictionary: List<Word> = emptyList(),
-    unLearningWord: List<Word>,
-    value: Int,
-    learningWord: Word,
-): List<Word> {
-    return if (unLearningWord.size - 1 < value) {
-        getAnswerChoices(dictionary, learningWord, value)
-    } else {
-        getAnswerChoices(unLearningWord, learningWord, value)
-    }
-}
-
-fun getAnswerChoices(dictionary: List<Word>, learningWord: Word, value: Int): List<Word> {
-    val tempDictionary = dictionary.filter { word -> word != learningWord }
-    val answers = tempDictionary.shuffled().take(value) + learningWord
-    return answers.shuffled()
-}
-
-fun getUnlearnedWords(dictionary: List<Word>): List<Word> {
-    return dictionary.filter { it.correctCount < WORD_LEARNED_LIMIT }
-}
-
-
 fun main() {
-    val dictionary = getDictionary()
+    val trainer = LearnWordsTrainer()
+
     while (true) {
         println(
             """
@@ -131,11 +15,51 @@ fun main() {
 
         when (userInput) {
             "1" -> {
-                learnWords(dictionary)
+                while (true) {
+                    val question = trainer.getNextQuestion()
+                    if (question == null) {
+                        println("Вы выучили все слова!")
+                        break
+                    } else {
+                        println(
+                            "Перевод слова <${
+                                question.correctAnswer.enWord.lowercase().replaceFirstChar { it.uppercase() }
+                            }>"
+                        )
+                        question.variants.forEach { it ->
+                            println(
+                                "${question.variants.indexOf(it) + 1}. ${
+                                    it.ruWord.lowercase().replaceFirstChar { it.uppercase() }
+                                }"
+                            )
+
+                        }
+                        println("0. Выход")
+                    }
+
+                    print("Введите ваш ответ: ")
+                    val userAnswer = readln().toIntOrNull()
+                    if (userAnswer == 0) break
+
+                    if (trainer.checkAnswer(userAnswer?.minus(1))) {
+                        println("Верно!")
+                    } else {
+                        println(
+                            "Не верно, перевод слова <" +
+                                    "${
+                                        question.correctAnswer.enWord.lowercase().replaceFirstChar { it.uppercase() }
+                                    }> " +
+                                    "это <${
+                                        question.correctAnswer.ruWord.lowercase().replaceFirstChar { it.uppercase() }
+                                    }>"
+                        )
+                    }
+                }
             }
 
             "2" -> {
-                getUserStatistics(dictionary)
+                val statistics = trainer.getUserStatistics()
+                println("Выучено ${statistics.learnedWords} из ${statistics.wordCount} слов | ${statistics.percentageLearned}%")
             }
 
             "0" -> {
@@ -143,9 +67,7 @@ fun main() {
                 break
             }
 
-            else -> {
-                println("Неверный ввод. Введите доступные значения: 1, 2 или 0.")
-            }
+            else -> println("Неверный ввод. Введите доступные значения: 1, 2 или 0.")
         }
     }
 }
